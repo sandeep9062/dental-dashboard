6335cad3307da455d13fcbf46dcfc9c17673a9b4
Sandeep Saini <sandeepsaini157219@gmail.com>
2025-12-09 12:02:27 +0530

    version 16t

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
+        hostname: "res.cloudinary.cloudinary.com",
       },
     ],
   },
